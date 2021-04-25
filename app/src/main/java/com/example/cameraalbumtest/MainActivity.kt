0a08ca86a7ceedb40df92681ff99476a13844d86
chris <1553801752@qq.com>
2021-04-25 21:24:10 +0800

    added from album ver2.0

--- app/src/main/java/com/example/cameraalbumtest/MainActivity.kt
+++ app/src/main/java/com/example/cameraalbumtest/MainActivity.kt
@@ -19,10 +19,12 @@ class MainActivity : AppCompatActivity() {
     val takePhoto = 1
     lateinit var imageUri: Uri
     lateinit var outputImage: File
+    val fromAlbum = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         takePhotoBtn.setOnClickListener {
             //创建File对象，用于存储拍照后的照片
             outputImage = File(externalCacheDir, "output_image.jpg")
@@ -40,6 +42,15 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
             startActivityForResult(intent, takePhoto)
         }
+
+        fromAlbumBtn.setOnClickListener {
+            //打开文件选择器
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+            intent.addCategory(Intent.CATEGORY_OPENABLE)
+            //指定只显示图片
+            intent.type = "image/*"
+            startActivityForResult(intent, fromAlbum)
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -52,7 +63,21 @@ class MainActivity : AppCompatActivity() {
                     imageView.setImageBitmap(rotateIfRequired(bitmap))
                 }
             }
+
+            fromAlbum -> {
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    data.data?.let { uri ->
+                        //将选择的图片显示
+                        val bitmap = getBitmapFromUri(uri)
+                        imageView.setImageBitmap(bitmap)
+                    }
+                }
+            }
         }
+    }
+
+    private fun getBitmapFromUri(uri: Uri) = contentResolver.openFileDescriptor(uri, "r")?.use {
+        BitmapFactory.decodeFileDescriptor(it.fileDescriptor)
     }
 
     private fun rotateIfRequired(bitmap: Bitmap): Bitmap {
